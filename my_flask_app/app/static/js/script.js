fd049e42a8e6f37d1320b7024fbfb73c8d1d1d6f
xavipg003 <xavipg003@gmail.com>
2024-07-21 12:54:02 +0200

    preview

--- my_flask_app/app/static/js/script.js
+++ my_flask_app/app/static/js/script.js
@@ -479,6 +479,7 @@ function generateVideo() {
         const aElement = document.createElement("a");
         aElement.setAttribute("download", "output.mp4");
         const href = window.URL.createObjectURL(data);
+        previewVideo(href)
         aElement.href = href;
         aElement.setAttribute("target", "_blank");
         aElement.click();
