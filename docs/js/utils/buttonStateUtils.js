55dc0f71fadd555c70df75a544fc7e01e50c14cb
DavidEsteso <davidesteso2003@gmail.com>
2025-01-16 21:31:37 +0100

    routes

--- docs/js/utils/buttonStateUtils.js
+++
@@ -1,8 +0,0 @@
-export const setButtonState = (button, state) => {
-    if (!button) return;
-    
-    button.disabled = !state;
-    button.style.opacity = state ? 1 : 0.3;
-    button.style.cursor = state ? 'pointer' : 'not-allowed';
-    button.style.pointerEvents = state ? 'auto' : 'none';
-};
