bc0a53412f9e79aab9f43d6f3ff319c2e8d0c974
DavidEsteso <davidesteso2003@gmail.com>
2025-01-16 21:35:30 +0100

    pages

---
+++ app/docs/js/components/fileManager.js
@@ -0,0 +1,79 @@
+import {appState} from '../stateManager.js';
+import { svgIcons } from '../config/svgIcons.js';
+
+function toggleButton(type, currentAction) {
+    const button = document.getElementById(type + "FileButton");
+    
+    if (!button) return;
+
+    button.replaceWith(button.cloneNode(true));
+    const newButton = document.getElementById(type + "FileButton");
+    
+    if (currentAction === 'remove') {
+        newButton.innerHTML = svgIcons.folderPlus;
+        newButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            fileManager.addFile(type);
+        });
+    } else {
+        newButton.innerHTML = svgIcons.trash;
+        newButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            fileManager.removeFile(type);
+        });
+    }
+    
+
+}
+
+
+export const fileManager = {
+    addFile(type) {
+        let fileInput = document.getElementById(type + "HiddenFileInput");
+        const linkInput = document.getElementById(type + "Link");
+        
+        if (!fileInput) {
+            fileInput = document.createElement("input");
+            fileInput.type = "file";
+            fileInput.accept = type === 'video' ? "video/*" : "audio/*";
+            fileInput.id = type + "HiddenFileInput";
+            fileInput.style.display = "none";
+            
+            fileInput.addEventListener("change", function() {
+                if (fileInput.files.length > 0) {
+                    const file = fileInput.files[0];
+                    appState.updateMedia(`${type}File`, file);
+                    appState.updateMedia(`${type}Link`, '');
+                    
+                    if (linkInput) {
+                        linkInput.value = file.name;
+                        linkInput.setAttribute('data-has-file', 'true');
+                    }
+                    toggleButton(type, 'add');
+                }
+            });
+            document.body.appendChild(fileInput);
+        }
+        fileInput.click();
+    },
+
+    removeFile(type) {
+        const fileInput = document.getElementById(type + "HiddenFileInput");
+        const linkInput = document.getElementById(type + "Link");
+        
+        if (fileInput) {
+            fileInput.remove();
+        }
+        
+        if (linkInput) {
+            linkInput.value = '';
+            linkInput.removeAttribute('data-has-file', 'true');
+
+        }
+        
+        appState.updateMedia(`${type}File`, null);
+        toggleButton(type, 'remove');
+    }
+};
---
+++ app/docs/js/config/constants.js
@@ -0,0 +1,6 @@
+export const CONSTANTS = {
+    MAX_FACTS: 5,
+    DEFAULT_LANGUAGE: 'en',
+    API_KEY: 'AIzaSyDDA5bfvWAdBPXn-K_kD9BeU1M-ju3VSeY',
+    MAX_TITLE_LENGTH: 20
+};
